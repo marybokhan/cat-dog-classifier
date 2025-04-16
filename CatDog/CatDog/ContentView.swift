4acc61d3182a4c67b7831c7d79669347250158ef
Mary Bokhan <marybokhan@yahoo.com>
2025-04-16 18:08:27 +0100

    Add photo picker

--- CatDog/CatDog/ContentView.swift
+++ CatDog/CatDog/ContentView.swift
@@ -1,8 +1,23 @@
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    
+    @State private var showPhotoPicker = false
+    @State private var selectedPhoto = [PhotosPickerItem]()
+    
     var body: some View {
-        self.addPhotoButton
+        Button(action: {
+            self.showPhotoPicker = true
+        }) {
+            self.addPhotoButton
+        }
+        .photosPicker(
+            isPresented: $showPhotoPicker,
+            selection: $selectedPhoto,
+            maxSelectionCount: 1,
+            matching: .images
+        )
     }
     
     private var addPhotoButton: some View {
